d417f274c95e01324f82ab0a97914531f72ce902
Gaganbaghelcyb <gaganba@cybage.com>
2024-03-22 13:25:55 +0530

    added frontend token management

--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -1,32 +1,37 @@
-import { Injectable } from '@angular/core';
+import { Injectable, PLATFORM_ID, Inject } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
-import { PLATFORM_ID, Inject } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
-    //test comment
-  private readonly AUTH_TOKEN_KEY = 'authToken';
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+export class AuthService{
+
+  private readonly AUTH_TOKEN_KEY = 'loginToken';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   setAuthToken(token: string): void {
-    // localStorage.setItem(this.AUTH_TOKEN_KEY, token);
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem(this.AUTH_TOKEN_KEY, token);
+    }
   }
 
-  // getAuthToken(): string | null {
-  //   // return localStorage.getItem(this.AUTH_TOKEN_KEY);
-  // }
+  getAuthToken(): string | null {
+    if (isPlatformBrowser(this.platformId)) {
+      return localStorage.getItem(this.AUTH_TOKEN_KEY);
+    }
+    return null;
+  }
 
-  // isLoggedIn(): boolean {
-  //   // return !!this.getAuthToken();
-  // }
+  isLoggedIn(): boolean {
+    return !!this.getAuthToken();
+  }
 
   logout(): void {
-    // localStorage.removeItem(this.AUTH_TOKEN_KEY);
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.removeItem(this.AUTH_TOKEN_KEY);
+    }
   }
-
-
 
 }
